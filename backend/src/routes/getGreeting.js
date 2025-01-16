00fdd26296e22d203989c7711169304325ff798d
John <johnodg369@gmail.com>
2025-01-16 11:51:58 +0000

    updated

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,14 @@
 const GREETING = 'Hello world!';
 
+const GREETINGS = [
+    "Hello docker world",
+    "Hello world",
+    "Nice to see you again"
+]
+
 module.exports = async (req, res) => {
+    let randIndex = Math.floor(Math.random() * GREETINGS.length)
     res.send({
-        greeting: GREETING,
+        greeting: GREETINGS[randIndex],
     });
 };
